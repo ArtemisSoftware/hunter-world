4b26f4870b0c9fb420ea7e87ea1ba7bc676f8774
gustavo <gustavo.isaac@gmail.com>
2026-05-15 18:54:30 +0100

    Main Buttons

---
+++ hud/hud.gd
@@ -0,0 +1,27 @@
+class_name  Hud extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_equipment_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inventory_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stats_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_skills_button_pressed() -> void:
+	pass # Replace with function body.
