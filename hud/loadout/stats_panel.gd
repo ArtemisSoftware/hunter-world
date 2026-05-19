2c5687870cec8a8282c9d4a003360a512603898a
gustavo <gustavo.isaac@gmail.com>
2026-05-19 09:30:45 +0100

    Stats Panel

---
+++ hud/loadout/stats_panel.gd
@@ -0,0 +1,27 @@
+class_name StatsPanel extends PanelContainer
+
+@onready var damage_label: Label = %DamageLabel
+@onready var hp_label: Label = %HpLabel
+@onready var velocity_label: Label = %VelocityLabel
+@onready var mana_label: Label = %ManaLabel
+@onready var critical_label: Label = %CriticalLabel
+@onready var critical_damage_label: Label = %CriticalDamageLabel
+
+@onready var current_level_label: Label = %CurrentLevelLabel
+@onready var current_points_label: Label = %CurrentPointsLabel
+
+@onready var strength_points_label: Label = %StrengthPointsLabel
+@onready var dexterity_points_label: Label = %DexterityPointsLabel
+@onready var inteligence_points_label: Label = %InteligencePointsLabel
+
+
+func _on_strength_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_dexterity_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inteligence_button_pressed() -> void:
+	pass # Replace with function body.
