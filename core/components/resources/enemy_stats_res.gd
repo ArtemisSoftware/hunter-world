6ff6b33019f34a302b1be28fe7411962a6d626d6
gustavo <gustavo.isaac@gmail.com>
2026-05-23 19:07:15 +0100

    Enemy scene

---
+++ core/components/resources/enemy_stats_res.gd
@@ -0,0 +1,5 @@
+class_name EnemyStatsRes extends Resource
+
+@export var damage: float = 1.0
+@export var max_health: float = 5.0
+@export var exp_amount: float = 5.0
---
+++ entities/enemies/enemy.gd
@@ -0,0 +1,21 @@
+class_name Enemy extends Area2D
+
+@export var stats: EnemyStatsRes
+
+@onready var selector: Sprite2D = $Selector
+@onready var health: Health = $Health
+
+
+
+func _ready() -> void:
+	health.setup(stats.max_health)
+	pass 
+	
+func select_enemy() -> void:
+	selector.show()
+	pass	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
